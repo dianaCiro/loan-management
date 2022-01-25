5ef780487a8fa9cc5a50cfcacaa936381d460aaa
diana.ciro <diana.ciro@globant.com>
2022-01-25 12:51:55 -0500

    MELI-001, initial project structure configured

---
+++ application/src/main/java/com/meli/loan/LoanApplication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.meli.loan;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author diana.ciro
+ *
+ */
+@SpringBootApplication
+public class LoanApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
